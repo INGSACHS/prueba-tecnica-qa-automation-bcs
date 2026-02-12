eb9ca9611fb65e4fe5227cd7ccd72aabde6f4895
Sergio Chiquillo <68573924+INGSACHS@users.noreply.github.com>
2026-02-12 00:35:46 -0500

    Add data-driven tests for Products API

---
+++ tests/data-driven/products-data.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '../../fixtures/test-fixtures';
+import { loadTestData } from '../../utils/dataLoader';
+import { ProductsData } from '../../types/testData';
+
+const data = loadTestData<ProductsData>('products.data.json');
+
+test.describe('Products API', () => {
+
+    data.forEach(d => {
+
+        test(d.testId, async ({ productsAPI }) => {
+            const response = await productsAPI.getProducts(d.token);
+            expect(response.status()).toBe(d.expectedStatus);
+        });
+
+    });
+
+});
