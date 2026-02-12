a796fed872bbd0056cdb8172f484e8cfbbe8b9d2
Sergio Chiquillo <68573924+INGSACHS@users.noreply.github.com>
2026-02-12 09:30:09 -0500

    Remove productsPage fixture from test fixtures
    Removed productsPage fixture from CreditFixtures type.

--- fixtures/test-fixtures.ts
+++ fixtures/test-fixtures.ts
@@ -1,14 +1,12 @@
 import { test as base } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { SimuladorPage } from '../pages/SimuladorPage';
-import { ProductsPage } from '../pages/ProductsPage';
 import { ProductsAPI } from '../api/ProductsAPI';
 import { SimuladorAPI } from '../api/SimuladorAPI';
 
 type CreditFixtures = {
     loginPage: LoginPage;
     simuladorPage: SimuladorPage;
-    productsPage: ProductsPage;
     productsAPI: ProductsAPI;
     simuladorAPI: SimuladorAPI;
 };
@@ -21,10 +19,6 @@ export const test = base.extend<CreditFixtures>({
 
     simuladorPage: async ({ page }, use) => {
         await use(new SimuladorPage(page));
-    },
-
-    productsPage: async ({ page }, use) => {
-        await use(new ProductsPage(page));
     },
 
     productsAPI: async ({ request }, use) => {
